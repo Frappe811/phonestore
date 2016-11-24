005578849ed24c11718e273951c3c223e6efb083
hiephc <falleninterface@gmail.com>
2016-11-24 16:08:13 +0700

    Add field description to phone, change price-button format. Update faker data

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161122082544) do
+ActiveRecord::Schema.define(version: 20161124085526) do
 
   create_table "phones", force: :cascade do |t|
     t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
     t.decimal  "price"
     t.string   "photo_url"
+    t.text     "description"
   end
 
   create_table "users", force: :cascade do |t|
