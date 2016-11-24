f310cc2c332378d8eafbade5075207006b4c1f29
Viet Hung <hungtvse61314@gmail.com>
2016-11-24 16:00:14 +0700

    Ignore secret and change dangerous values with secret keys

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -247,7 +247,7 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
-  config.omniauth :facebook, "338342163207538", "7f48d1c101e3e9863fe28010c3baa791"
+  config.omniauth :facebook, Rails.application.secrets.facebook_app_id, Rails.application.secrets.facebook_app_secret
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
