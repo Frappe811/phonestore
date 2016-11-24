4b21bdaabcd474d08f73d95e8d9ba764c3cb7a16
Viet Hung <hungtvse61314@gmail.com>
2016-11-24 15:26:30 +0700

    Create Login with facebook link and update design

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161115065143) do
+ActiveRecord::Schema.define(version: 20161124063709) do
 
   create_table "phones", force: :cascade do |t|
     t.string   "name"
@@ -32,6 +32,8 @@ ActiveRecord::Schema.define(version: 20161115065143) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "provider"
+    t.string   "uid"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
