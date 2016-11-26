dbcd3efde86f37187383c83a04356f00678e8aa9
Frappe811 <monichan811@gmail.com>
2016-11-26 17:19:35 +0700

    Change Status for User when they Sign in system

--- db/migrate/20161126101641_create_categories.rb
+++ db/migrate/20161126101641_create_categories.rb
@@ -1,7 +1,7 @@
 class CreateCategories < ActiveRecord::Migration
   def change
     create_table :categories do |t|
-      t.string :BrandName
+      t.string :brand_name
 
       t.timestamps null: false
     end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161122082544) do
+ActiveRecord::Schema.define(version: 20161126101641) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "brand_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "phones", force: :cascade do |t|
     t.string   "name"
