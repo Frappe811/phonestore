c2e0865732ec684ead65fcd96265b21b8a8c6e01
Frappe811 <monichan811@gmail.com>
2016-11-27 16:12:40 +0700

    Install figaro

--- Gemfile
+++ Gemfile
@@ -25,6 +25,7 @@ gem 'jbuilder', '~> 2.0'
 # bundle exec rake doc:rails generates the API under doc/api.
 gem 'sdoc', '~> 0.4.0', group: :doc
 gem 'devise'
+gem 'figaro'
 gem 'font-awesome-rails'
 gem 'growlyflash'
 gem 'omniauth-facebook'
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -247,7 +247,7 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
-  config.omniauth :facebook, Rails.application.secrets.facebook_app_id, Rails.application.secrets.facebook_app_secret
+  config.omniauth :facebook, ENV['facebook_app_id'], ENV['facebook_app_secret']
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
