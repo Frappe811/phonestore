ef7efc8c440ba52e8b7ab13c5762ef14ee28e33c
hiephc <falleninterface@gmail.com>
2016-11-29 15:17:32 +0700

    add phone's specification field to phone table, fake data; update phone show

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161126105625) do
+ActiveRecord::Schema.define(version: 20161129070923) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,16 @@ ActiveRecord::Schema.define(version: 20161126105625) do
     t.decimal  "price"
     t.string   "photo_url"
     t.text     "description"
+    t.string   "display"
+    t.string   "os"
+    t.string   "back_camera"
+    t.string   "font_camera"
+    t.string   "cpu"
+    t.string   "ram"
+    t.string   "storage"
+    t.string   "sp_storage"
+    t.string   "sim"
+    t.string   "battery"
   end
 
   create_table "users", force: :cascade do |t|
--- spec/factories/phones.rb
+++ spec/factories/phones.rb
@@ -4,5 +4,15 @@ FactoryGirl.define do
     photo_url { Faker::Placeholdit.image('221x221') }
     price { Faker::Number.decimal(2) }
     description { Faker::Lorem.paragraph }
+    display { Faker::Name.last_name }
+    os { Faker::Name.last_name }
+    back_camera { Faker::Name.last_name }
+    font_camera { Faker::Name.last_name }
+    cpu { Faker::Name.last_name }
+    ram { Faker::Name.last_name }
+    storage { Faker::Name.last_name }
+    sp_storage { Faker::Name.last_name }
+    sim { Faker::Name.last_name }
+    battery { Faker::Name.last_name }
   end
 end
