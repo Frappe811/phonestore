7d42842af39e128253ac50a0171fb2a84503b190
hiephc <falleninterface@gmail.com>
2016-11-30 14:27:14 +0700

    update fake data

--- spec/factories/phones.rb
+++ spec/factories/phones.rb
@@ -4,15 +4,15 @@ FactoryGirl.define do
     photo_url { Faker::Placeholdit.image('221x221') }
     price { Faker::Number.decimal(2) }
     description { Faker::Lorem.paragraph }
-    display { Faker::Name.last_name }
-    os { Faker::Name.last_name }
-    back_camera { Faker::Name.last_name }
-    font_camera { Faker::Name.last_name }
-    cpu { Faker::Name.last_name }
-    ram { Faker::Name.last_name }
-    storage { Faker::Name.last_name }
-    sp_storage { Faker::Name.last_name }
-    sim { Faker::Name.last_name }
-    battery { Faker::Name.last_name }
+    display { "Super AMOLED #{Faker::Number.between(3,6)} inch"}
+    os { "Android #{Faker::Number.between(4,6)}" }
+    back_camera { "#{Faker::Number.between(2,16)} MP" }
+    font_camera { "#{Faker::Number.between(2,5)} MP" }
+    cpu { "Snapdragon #{Faker::Number.number(4)} #{Faker::Number.number(1)} Core" }
+    ram { "#{Faker::Number.between(1,4)} GB" }
+    storage { "#{Faker::Number.number(2)} GB" }
+    sp_storage { "#{Faker::Number.number(3)} GB" }
+    sim { "#{Faker::Number.between(1, 2)} Sim" }
+    battery { "#{Faker::Number.number(4)} mAh" }
   end
 end
