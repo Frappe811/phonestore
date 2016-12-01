5ccca85f1a4fa200d8febb2c5187e087b0f5a39c
Frappe811 <monichan811@gmail.com>
2016-12-01 23:33:35 +0700

    Finish Search function

--- app/controllers/phones_controller.rb
+++ app/controllers/phones_controller.rb
@@ -1,11 +1,17 @@
 class PhonesController < ApplicationController
   def index
     category = Category.find_by_id(params[:id])
+    keyword = params[:q]
     @category = category
-    if category == nil
-      @phones = Phone.page(params[:page]).per(12)
+    if keyword == nil
+      if category == nil
+        @phones = Phone.page(params[:page]).per(12)
+      else
+        @phones = category.phones.page(params[:page]).per(12)
+      end
     else
-      @phones = category.phones.page(params[:page]).per(12)
+      @phones = Phone.search_by_name(keyword).page(params[:page]).per(12)
+      # count = Phone.search_by_name(keyword).count
     end
   end
 
--- app/models/phone.rb
+++ app/models/phone.rb
@@ -2,4 +2,5 @@ class Phone < ActiveRecord::Base
   validates :name, :photo_url, :price, presence: true
   belongs_to :category
   include PgSearch
+  pg_search_scope :search_by_name, :against => :name
 end
