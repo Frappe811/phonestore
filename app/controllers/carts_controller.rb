b37976414cc5e0d0dc687bcffdabc0fcbfe8c3e9
hiephc <falleninterface@gmail.com>
2016-12-05 17:08:51 +0700

    update code orderhistory, cart page

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -2,34 +2,36 @@ class CartsController < ApplicationController
 
   def add
     phone = Phone.find(params[:phone_id])
-    cart = Cart.find_by(id: session[:cart_id])
+    @cart = Cart.find_by(id: session[:cart_id])
 
-    if cart.nil?
-      cart = Cart.create
+    if @cart.nil?
+      @cart = Cart.create
       session[:cart_id] = cart.id
     end
-    cart.add(phone)
-    redirect_to :back
+    @cart.add(phone)
+
+    respond_to do |format|
+      format.html { redirect_to :back }
+      format.js { render :add }
+    end
+
   end
 
   def remove
     phone = Phone.find(params[:phone_id])
-    cart = Cart.find_by(id: session[:cart_id])
+    @cart = Cart.find_by(id: session[:cart_id])
 
-    if cart != nil
-      cart.remove(phone)
-
+    if @cart != nil
+      @cart.remove(phone)
+      respond_to do |format|
+        format.js { render :add }
+      end
     end
-    redirect_to carts_path
 
   end
 
   def show
-
       @cart = current_cart
-      if @cart.present?
-
-      end
   end
 
   private
